ec1fcf1d00f6a2ecb61d0d7857b95233247f646b
Петров Олег <araragikyn@hotmail.com>
2020-04-09 16:12:59 +0800

    Начал писать схему классов

---
+++ HomeWork_13/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork_13.Models
+{
+    public abstract class Account 
+    {
+        static List<long> idList;
+
+        static Account()
+        {
+            idList = new List<long>();
+        }
+
+        protected string owner;
+        protected long id;
+        protected double balance;
+        protected long 
+    }
+}
---
+++ HomeWork_13/Models/Bank.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace HomeWork_13.Models
+{
+    class Bank
+    {
+    }
+}
