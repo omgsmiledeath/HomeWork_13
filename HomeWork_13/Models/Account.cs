00c7a87e88e5e7403890f348eb780a4674bc70aa
Holiday <Holiday@DESKTOP-54GAQ21>
2020-05-11 22:51:07 +0800

    1

--- HomeWork_13/Models/Account.cs
+++ HomeWork_13/Models/Account.cs
@@ -15,8 +15,8 @@ namespace HomeWork_13.Models
         }
         private double balance;//текущий баланс на счету
         private long id; // ID счета
-        private double interestRate;
-
+        protected double interestRate;
+        protected List<string> LogTransaction;
         public double Balance { get => balance; set => balance = value; }
        
         /// <summary>
@@ -30,6 +30,30 @@ namespace HomeWork_13.Models
             interestRate = 4.5;
             id = idList.Count;
             idList.Add(id);
+            LogTransaction = new List<string>();
+        }
+
+        public virtual bool Withdraw(Account target,double amount)
+        {
+            if((Balance - amount)>=0)
+            {
+                Balance -= amount;
+                target.Deposit(amount);
+                LogTransaction.Add($"Withdrawn {amount} to {target} at {DateTime.Now} ");
+                return true;
+            }
+            return false;
+        }
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+            LogTransaction.Add($"Added {amount} at {DateTime.Now}");
+        }
+
+        public override string ToString()
+        {
+            return $"Account number :{this.id}";
         }
     }
 }
---
+++ HomeWork_13/Models/SaveAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_13.Models
+{
+    public class SaveAccount : Account
+    {
+        private DateTime startInvestmentDate;
+        private DateTime completeInvestmentDate;
+        private double interestBalance;
+        public SaveAccount(double amount) : base(amount)
+        {
+            
+        }
+
+        
+
+        public void ChangeInterestRate(double rate)
+        {
+            interestRate = rate;
+        }
+
+        public bool StartInvestment(double amount)
+        {
+            if((Balance-amount)>=0)
+            {
+                startInvestmentDate = DateTime.Now;
+                completeInvestmentDate = DateTime.Now.AddMonths(12);
+                interestBalance += amount;
+                Balance -= amount;
+                LogTransaction.Add($"Investment {amount}");
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+
+
+
+}
