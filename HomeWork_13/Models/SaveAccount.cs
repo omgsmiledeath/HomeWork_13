a58620a002291134e89019b6a481e02b85989b29
Holiday <Holiday@DESKTOP-54GAQ21>
2020-09-14 23:21:00 +0800

    Доделать вклады , затуп с датами при загрузке сохраняются id

--- HomeWork_13/Models/SaveAccount.cs
+++ HomeWork_13/Models/SaveAccount.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HomeWork_13.Models
 {
     public class SaveAccount : Account
     {
 
-        enum TypeInvestment
+        public enum TypeInvestment
         {
             WithCapitalization,
             WithoutCapitalization
@@ -17,7 +19,7 @@ namespace HomeWork_13.Models
 
         private DateTime startInvestmentDate;
         private DateTime completeInvestmentDate;
-
+        private double capitalizeRate = 0.05;
         private TypeInvestment currentInvestment;
         private int mounts;
         private double interestBalance;
@@ -50,7 +52,7 @@ namespace HomeWork_13.Models
 
         public double InterestBalance { get => interestBalance; set => interestBalance = value; }
         public double InterestRate { get => interestRate;  }
-        private TypeInvestment CurrentInvestment { get => currentInvestment; set => currentInvestment = value; }
+        public TypeInvestment CurrentInvestment { get => currentInvestment; set => currentInvestment = value; }
 
         public SaveAccount(double amount,double bonusInterestRate=0) : base(amount,AccountTypes.Debit)
         {
@@ -76,7 +78,7 @@ namespace HomeWork_13.Models
                 CompleteInvestmentDate = DateTime.Now.AddMonths(Mounts);
                 InterestBalance += amount;
                 Balance -= amount;
-                LogTransaction.Add($"Investment {amount} start at {StartInvestmentDate} {InterestBalance}");
+                LogTransaction.Add($"Investment {amount} start at {StartInvestmentDate} {(flag ? "with capitalization" :"without capitalization")}");
                 InvestitionProcess = true;
                 return true;
             }
@@ -87,8 +89,8 @@ namespace HomeWork_13.Models
 
         public bool CheckInvestment()
         {
-            if(DateTime.Now>CompleteInvestmentDate)
-            {
+            //if(DateTime.Now>CompleteInvestmentDate)
+            //{
                 switch(CurrentInvestment)
                 {
                     case (TypeInvestment.WithCapitalization):
@@ -100,33 +102,70 @@ namespace HomeWork_13.Models
                 }
                 
                 return true;
-            }
-            return false;
+            //}
+            //return false;
         }
 
+        /// <summary>
+        /// Метод расчета вклада с ежемесячной капитализациией
+        /// </summary>
         private void CompleteInvestment()
         {
-            double mountInterest = 0;
-            double startInterestBalance =InterestBalance;
-            for (int i = 0; i < Mounts; i++)
+            
+                
+                var span =   CompleteInvestmentDate - DateTime.Now;
+                var months = Math.Ceiling(span.TotalDays / 30.4);
+            if (months >= 12)
             {
-                mountInterest = InterestBalance * (InterestRate / 100);
-                InterestBalance += mountInterest;
-                LogTransaction.Add($"mountInterest - {mountInterest} interest balance - {InterestBalance}");
-                mountInterest = 0;
+                LogTransaction.Add($"Investment less than a month, denied");
+                return;
             }
-
-
-            interestRate += InterestRate / 10 * 5;
-            Balance += InterestBalance;
-            LogTransaction.Add($"Investment complete with {InterestBalance}");
-            InterestBalance = 0;
+            else
+            {
+                months = 11 - months;
+                
+                InterestBalance = InterestBalance * Math.Pow((1 + InterestRate / 100 / Mounts), months );
+                Balance += InterestBalance;
+                LogTransaction.Add($"Investment complete for {months} months with {InterestBalance}");
+                InterestBalance = 0;
+                startInvestmentDate = DateTime.Now;
+            }
             
         }
 
+        /// <summary>
+        /// Метод расчета вклада без капитализации с возможность получения прибыли за уже пройденные месяцы
+        /// </summary>
         private void CompleteSimpleInvestment()
         {
 
+
+
+
+            var span = CompleteInvestmentDate - DateTime.Now;
+                var months = Math.Ceiling(span.TotalDays / 30.4);
+
+
+
+            if (months >= 12)
+            {
+                LogTransaction.Add($"Investment less than a month, denied");
+                return;
+            }
+            else
+            {
+                months = 11 - months;
+                double monthInterest = 0;
+
+                for (var i = 1; i < months; i++)
+                {
+                    monthInterest += InterestBalance * InterestRate / 100 / Mounts;
+                    LogTransaction.Add($"Get investment for {i} month = { InterestBalance * InterestRate / 100 / Mounts}");
+                }
+                InterestBalance += monthInterest;
+                Balance += InterestBalance;
+                startInvestmentDate = DateTime.Now;
+            }
         }
 
 
